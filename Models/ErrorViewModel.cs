7408a76b916fd12a4c2cfc31815205e2c82d1b04
Amiralahiani <amira.lahiani@insat.ucar.tn>
2026-02-06 00:41:09 +0100

    Remove duplicate root project folders

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RealEstateAdmin.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
